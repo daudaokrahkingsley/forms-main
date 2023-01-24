0c3bf8a6acd8c6e7add55091d6a7400053099ce5
OKRAH DAUDA KINGSLEY(ODK) <99811118+daudaokrahkingsley@users.noreply.github.com>
2023-01-24 15:07:55 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,51 +1,62 @@
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-	<title>Insert</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing page</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/8da83a5fd6.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
-
+ 
 <body>
-	<center>
-		<?php
+    <div class="main-content">
+        <div class="img-fluid">
+            <img src="images/450-removebg-preview (1).png" alt="a picture of a boy">
+        </div>
 
-		 
-		$conn = mysqli_connect("localhost", "root", "", "personal_db");
-		
-		// Check connection
-		if($conn === false){
-			die("ERROR: Could not connect. "
-				. mysqli_connect_error());
-		}
-		
-		 
-		$fname = $_REQUEST['fname'];
-		$sname = $_REQUEST['sname'];
-		$phone = $_REQUEST['phone'];
-		$gender = $_REQUEST['gender'];
-		 
-		
-		 
-		$sql = "INSERT INTO  personaldetails_tb VALUES ('$fname',
-			'$sname','$phone','$gender')";
-		
-		if(mysqli_query($conn, $sql)){
-			echo "<h3>data stored in a database successfully."
-			. " Please browse your localhost php my admin"
-				. " to view the updated data</h3>";
+        <h1>Welcome</h1>
+        <button class="btn-primary">Continue</button>
+        <a href="forms.html" class="btn-primary btn-sm">Continue</a>
+    </div>
 
-			//echo nl2br("\n$first_name\n $last_name\n "
-				//. "$gender\n $address\n $email");
-		} else{
-			echo "ERROR: Sorry $sql. "
-				. mysqli_error($conn);
-		}
-		
-		// Close connection
-		mysqli_close($conn);
-		?>
-	</center>
+    <div class="overlay hide"></div>
+
+    <div class="forms hide">
+        <h1>Personal Details</h1>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="close-modal">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        <form class="form-details" action="insert.php" method="post">
+            <div class="input-field">
+                <span class="icon"><i class=" fa-solid fa-user"></i></span> <input type="text" name="fname" placeholder="Enter Firstname" class="form-text" maxlength="20"><br>
+            </div>
+
+            <div class="input-field">
+                <span class="icon"><i class=" fa-solid fa-user"></i></span><input type="text" name="sname" placeholder=" Enter Surname" class="form-text" maxlength="20"><br>
+            </div>
+
+            <div class="input-field">
+                <span class="icon"><i class="fa-solid fa-phone"></i></span><input type="tel" name="phone" placeholder="e.g. +233 000 000 0000" class="form-text"><br>
+            </div>
+
+            <div class="input-field">
+                <span class="icon"><i class=" fa-solid fa-user"></i></span> <select name="gender" id="gender" class="form-text">
+                    <option></option>
+                    <option name="gender">Male</option>
+                    <option  name="gender" >Female</option>
+              </select><br>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    <script src="script.js"></script>
 </body>
 
 </html>
